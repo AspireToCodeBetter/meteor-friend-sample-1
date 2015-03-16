1ef43517c84cfafb292115c826a34124faa264dd
Santosh Srinivas <santosh.srinivas@gmail.com>
2015-03-16 10:34:56 +0530

    Added user friendship check in the publication

--- server/publications/userProfile.js
+++ server/publications/userProfile.js
@@ -3,7 +3,42 @@
 Meteor.publish("userProfile", function(userId) {
   console.log("In the user profile subscription for: " + userId);
   console.log("In the user profile subscription, the logged in user is: " + this.userId);
+
+  check(userId, String);
   // only publish the fields you really need
   var fields = {username: 1, emails: 1, profile: 1};
-  return Meteor.users.find(userId,{fields: fields});
+  var self = this;
+
+  // modify this as needed
+  var transform = function(user) {
+    console.log("In the user profile subscription, checking for transformation, the logged in user is: " + self.userId);
+    console.log("Friends are: " + user.profile.friends);
+    user.profile.friends = _.filter(user.profile.friends, function(userId) {return userId === self.userId});
+    console.log("Friends are: " + user.profile.friends);
+    console.log("Type of Friends are: " + (user.profile.friends.length > 0));
+    user.isFriend = (user.profile.friends.length > 0) ? true : false;
+    return user;
+  };
+
+
+
+  var handle = Meteor.users.find(userId, {fields: fields}).observe({
+    added: function (user) {
+      self.added('users', user._id, transform(user));
+    },
+
+    changed: function (user) {
+      self.changed('users', user._id, transform(user));
+    },
+
+    removed: function (user) {
+      self.removed('users', user._id);
+    }
+  });
+
+  this.ready();
+
+  this.onStop(function() {
+    handle.stop();
+  });
 });
