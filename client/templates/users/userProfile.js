4cbe5071ace79e6d507b45af92b5248e8207da1b
Santosh Srinivas <santosh.srinivas@gmail.com>
2015-03-16 23:42:42 +0530

    Added raix-handlebar helpers. Demonstrated a working prototype of the friend functionality

--- client/templates/users/userProfile.js
+++ client/templates/users/userProfile.js
@@ -7,6 +7,8 @@ Template.userProfile.events({
             console.log("Going to send friend request to: " + targetUserId);
 			Meteor.users.update({_id: targetUserId},
             	{$addToSet: {"profile.friends": Meteor.userId()}});
+            Meteor.users.update({_id: Meteor.userId()},
+                {$addToSet: {"profile.friends": targetUserId}});
         },
     'click #removeFriend':
         function(e) {
@@ -14,6 +16,8 @@ Template.userProfile.events({
             console.log("Going to remove friend request from: " + targetUserId);
 			Meteor.users.update({_id: targetUserId},
             	{$pull: {"profile.friends": Meteor.userId()}});
+            Meteor.users.update({_id: Meteor.userId()},
+                {$pull: {"profile.friends": targetUserId}});            
         }
 
 });
