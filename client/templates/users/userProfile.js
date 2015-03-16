971eef94acedece58a90e5157271e3277948f0b5
fountainhead@nitrous <santosh@mypad.in>
2015-03-16 12:29:54 -0400

    added the removing from friends functionality

--- client/templates/users/userProfile.js
+++ client/templates/users/userProfile.js
@@ -7,5 +7,13 @@ Template.userProfile.events({
             console.log("Going to send friend request to: " + targetUserId);
 			Meteor.users.update({_id: targetUserId},
             	{$addToSet: {"profile.friends": Meteor.userId()}});
+        },
+    'click #removeFriend':
+        function(e) {
+			var targetUserId = $(e.currentTarget).attr("data-id");
+            console.log("Going to remove friend request from: " + targetUserId);
+			Meteor.users.update({_id: targetUserId},
+            	{$pull: {"profile.friends": Meteor.userId()}});
         }
+
 });
